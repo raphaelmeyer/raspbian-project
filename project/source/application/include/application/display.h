48984a32b183e8517b6b0e162537c568bb9ecbe6
raphael meyer <raphael.akira.meyer@gmail.com>
2015-09-16 23:28:02 +0200

    Add test skeleton for display

---
+++ project/source/application/include/application/display.h
@@ -0,0 +1,10 @@
+#ifndef display_h
+#define display_h
+
+class Display {
+  public:
+
+};
+
+#endif
+
---
+++ project/source/unit-tests/display.cc
@@ -0,0 +1,10 @@
+#include <gmock/gmock.h>
+
+#include <application/display.h>
+
+TEST(display, is_initialized_with_a_sequence_of_commands) {
+
+  Display testee;
+
+}
+
