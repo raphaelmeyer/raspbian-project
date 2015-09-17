7f71605e3859489eae730f5f22059875b3c1d0af
raphael meyer <raphael.akira.meyer@gmail.com>
2015-09-17 22:09:50 +0200

    Add all commands for initialization sequence

--- project/source/unit-tests/display.cc
+++ project/source/unit-tests/display.cc
@@ -9,19 +9,6 @@ class Dogm163_Mock : public iDogm163 {
     MOCK_METHOD1(write_command, bool(std::uint8_t command));
 };
 
-/*
-Function Set      0 0 0 0 1 1 1 0 0 1 $39 8-Bit Datenlänge, 2 Zeilen, Instruction table 1
-Bias Set          0 0 0 0 0 1 0 1 0 1 $15 BS: 1/5, 3-zeiliges LCD
-Power Control     0 0 0 1 0 1 0 1 0 1 $55 Booster ein, Kontrast C5, C4setzen
-Follower Control  0 0 0 1 1 0 1 1 1 0 $6E Spannungsfolger und Verstärkung setzen
-Contrast Set      0 0 0 1 1 1 0 0 1 0 $72 Kontrast C3, C2, C1 setzen
-Function Set      0 0 0 0 1 1 1 0 0 0 $38 Zurück zur Instruction table 0
-Display ON/OFF    0 0 0 0 0 0 1 1 1 1 $0F Display ein, Cursor ein, Cursor blinken
-Clear Display     0 0 0 0 0 0 0 0 0 1 $01 Display löschen, Cursor Home
-Entry Mode Set    0 0 0 0 0 0 0 1 1 0 $06 Cursor Auto-Increment
-
-*/
-
 TEST(display, is_initialized_with_a_sequence_of_commands)
 {
   Dogm163_Mock dogm163;
@@ -38,7 +25,24 @@ TEST(display, is_initialized_with_a_sequence_of_commands)
   EXPECT_CALL(dogm163, write_command(0x55))
     .WillOnce(Return(true));
 
-  testee.init();
+  EXPECT_CALL(dogm163, write_command(0x6E))
+    .WillOnce(Return(true));
 
+  EXPECT_CALL(dogm163, write_command(0x72))
+    .WillOnce(Return(true));
+
+  EXPECT_CALL(dogm163, write_command(0x38))
+    .WillOnce(Return(true));
+
+  EXPECT_CALL(dogm163, write_command(0x0F))
+    .WillOnce(Return(true));
+
+  EXPECT_CALL(dogm163, write_command(0x01))
+    .WillOnce(Return(true));
+
+  EXPECT_CALL(dogm163, write_command(0x06))
+    .WillOnce(Return(true));
+
+  testee.init();
 }
 
